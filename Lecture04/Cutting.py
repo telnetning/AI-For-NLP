790a6d20eabfc3ff826bdad4075b21e5513ee4ff
telnetning <telnetning@gmail.com>
2019-04-25 23:51:10 +0800

    【Lecture04】Parse Cutting Solution

--- Lecture04/Cutting.py
+++ Lecture04/Cutting.py
@@ -23,10 +23,38 @@ def r(n):
     r(n) = max(p(n), r(n-1) + r(1), ....,r(1)+r(n-1))
     :return:
     """
-    return max([ prices[n-1] ] + [r(n-i) + r(i) for i in range(1, n)])
+    return max([ prices[n] ] + [r(n-i) + r(i) for i in range(1, n)])
 
 print(r(100))
 
+#####parse solution#####
+solution = {}
+@lru_cache(maxsize=2**10) # 装饰器缓存，不缓存n足够大时会递归过深报错
+def rr(n):
+    """
+    r(n) = max(p(n), r(n-1) + r(1), ....,r(1)+r(n-1))
+    :return:
+    """
+    max_price, split_point =  max([(prices[n], 0)] + [(rr(n-i) + rr(i), i) for i in range(1,n)], key=lambda x :x[0])
+
+    solution[n] = (split_point, n - split_point)
+    return max_price
+
+print(solution[123]) # (3, 120)
+
+def not_cut(split): return  split == 0
+
+def parse_solution(target_length, revenue_solution):
+
+    if target_length not in revenue_solution.keys(): rr(target_length) # 如果还不存在子问题的解
+
+    left, right = revenue_solution[target_length]
+
+    if not_cut(left): return [right] # [0,2] 这种表示不需要再分了
+
+    return parse_solution(left, revenue_solution) + parse_solution(right, revenue_solution)
+
+print(parse_solution(15, solution)) # [2,3,10]
 """
 动态规划可以分解成三个部分：
 1. 子问题重复
